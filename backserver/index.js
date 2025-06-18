1f3c17f37d75041fe08aacdc5086e09c16d77b4a
masihrnc <masihahmad.rnc@gmail.com>
2025-06-18 18:34:13 +0530

    Add mew video socket io

--- backserver/index.js
+++ backserver/index.js
@@ -71,4 +71,92 @@ io.on("connection", (socket) => {
   });
 })
 
+io.on("connection", (socket) => {
+  console.log(`New Socket Connection: ${socket.id}`);
+
+  socket.on("room:join", (data) => {
+    const { username, displayPicture, platform } = data;
+    users.set(socket.id, {
+      socketId: socket.id,
+      username,
+      displayPicture,
+      platform,
+      joinedAt: new Date(),
+      isConnected: false,
+    });
+    io.emit("refresh:user-list");
+  });
+
+  socket.on("peer:call", (data) => {
+    const { to, offer } = data;
+    socket.to(to).emit("peer:incomming-call", {
+      from: socket.id,
+      user: users.get(socket.id),
+      offer,
+    });
+  });
+
+  socket.on("peer:call:accepted", (data) => {
+    const { to, offer } = data;
+    if (users.has(to)) {
+      //@ts-ignore
+      users.get(to)?.isConnected = true;
+    }
+    if (users.has(socket.id)) {
+      //@ts-ignore
+      users.get(socket.id)?.isConnected = true;
+    }
+
+    socket.to(to).emit("peer:call:accepted", {
+      from: socket.id,
+      user: users.get(socket.id),
+      offer,
+    });
+
+    const whiteboardID = uuidV4();
+    io.to([to, socket.id]).emit("whiteboard:id", { whiteboardID });
+
+    io.emit("refresh:user-list");
+  });
+
+  socket.on("peer:negotiate", (data) => {
+    const { to, offer } = data;
+
+    socket.to(to).emit("peer:negotiate", { from: socket.id, offer });
+  });
+
+  socket.on("peer:negosiate:result", (data) => {
+    const { to, offer } = data;
+
+    socket.to(to).emit("peer:negosiate:result", { from: socket.id, offer });
+  });
+
+  socket.on("whiteboard:drawing", (data) => {
+    const { to } = data;
+
+    socket.to(to).emit("whiteboard:data", { from: socket.id, data: data });
+  });
+
+  socket.on("chat:message", (data) => {
+    const { to, message } = data;
+    socket.emit("chat:message", {
+      from: socket.id,
+      message,
+      self: true,
+      user: users.get(socket.id),
+    });
+    socket.to(to).emit("chat:message", {
+      from: socket.id,
+      message,
+      user: users.get(socket.id),
+    });
+  });
+
+  socket.on("disconnect", () => {
+    users.delete(socket.id);
+    io.emit("user-disconnected", { socketId: socket.id });
+    io.emit("refresh:user-list");
+  });
+});
+
 
